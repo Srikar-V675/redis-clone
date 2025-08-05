0f34a6826995ce85e921a1569749a890b5370319
Srikar V <srikarvuchiha@gmail.com>
2025-08-05 18:30:01 +0530

    Respond to Multiple Pings - A1

--- app/main.py
+++ app/main.py
@@ -9,7 +9,9 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _ = server_socket.accept() # wait for client
-    connection.sendall(b"+PONG\r\n")
+    data = connection.recv(1024)
+    if data == "PING":
+        connection.sendall(b"+PONG\r\n")
     
 
 
