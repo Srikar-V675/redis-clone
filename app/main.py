326543931a503b2dee0ed4e9e5c34b8921c6dd79
Srikar V <srikarvuchiha@gmail.com>
2025-08-05 19:52:15 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,12 @@
 import socket  # noqa: F401
+import threading
+
+
+def handle_client(connection: socket):
+    while True:
+        data = connection.recv(1024)
+        if not data: break
+        connection.sendall(b"+PONG\r\n")
 
 
 def main():
@@ -6,15 +14,16 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
-    #
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept() # wait for client
-    data = connection.recv(1024)
-    while data:
-        connection.sendall(b"+PONG\r\n")
-        data = connection.recv(1024)
     
-
+    # server
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    
+    # accepting connections: new thread for connection
+    while True:
+        conn, _ = server_socket.accept()
+        thread = threading.Thread(target=handle_client, args=(conn,))
+        thread.start()
+    
 
 if __name__ == "__main__":
     main()
