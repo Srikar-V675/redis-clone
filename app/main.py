40703c8c342d293bde30129f6caee6efcc980f2f
Srikar V <srikarvuchiha@gmail.com>
2025-08-30 06:52:17 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,56 +2,165 @@ import socket  # noqa: F401
 import threading
 import time 
 
-DATA = {}
-EXPIRY = {}
 
-def parser(connection: socket):
-    request = connection.recv(1024)
-    data = request.decode().splitlines()
-    parsed_data = []
-    for i in range(2, len(data), 2):
-        parsed_data.append(data[i])
-    return parsed_data
-
-
-def handle_client(connection: socket):
-    while True:
-        parsed_data = parser(connection=connection)
-        if parsed_data[0].lower() == "echo":
-            s = parsed_data[1]
-            connection.sendall(f"${len(s)}\r\n{s}\r\n".encode())
-        elif parsed_data[0].lower() == "set":
-            DATA[parsed_data[1]] = parsed_data[2]
-            if len(parsed_data) > 3 and parsed_data[3].lower() == "px":
-                EXPIRY[parsed_data[1]] = (time.time() * 1000) + int(parsed_data[4])
-            connection.sendall(b"+OK\r\n")
-        elif parsed_data[0].lower() == "get":
-            val = DATA[parsed_data[1]]
-            if parsed_data[1] in EXPIRY and (time.time()*1000) > EXPIRY[parsed_data[1]]:
-                connection.sendall(b"$-1\r\n")
+class Parser:
+    def __init__(self):
+        pass
+    
+    
+    @staticmethod
+    def parse_request(request: bytes):
+        data = request.decode().split("\r\n")
+        num_elements = int(data[0][1:])
+        tokens = []
+        
+        i = 1
+        while i < len(data) and len(tokens) < num_elements:
+            if data[i].startswith("$"):
+                length = int(data[i][1:])
+                tokens.append(data[i+1])
+                i += 2
             else:
-                connection.sendall(f"${len(val)}\r\n{val}\r\n".encode())
-        elif parsed_data[0].lower() == "rpush":
-            if parsed_data[1] in DATA:
-                DATA[parsed_data[1]].append(parsed_data[2])
+                i += 1
+        
+        cmd, *args = tokens
+        return cmd.upper(), args
+    
+    
+    @staticmethod
+    def format_response(response):
+        if response is None:
+            return "$-1\r\n"
+        elif isinstance(response, str):
+            if response == "OK" or response == "PONG":
+                return f"+{response}\r\n"
             else:
-                DATA[parsed_data[1]] = [parsed_data[2]]
-            connection.sendall(f":{len(DATA[parsed_data[1]])}\r\n".encode())
+                return f"${len(response)}\r\n{response}\r\n"
+        elif isinstance(response, int):
+            return f":{response}\r\n"
+        elif isinstance(response, list):
+            pass
         else:
-            connection.sendall(b"+PONG\r\n")
+            return "+PONG\r\n"
 
+
+class CommandHandler:
+    def __init__(self, datastore):
+        self.datastore = datastore
+        self.commands = {
+            "ECHO": self.handle_echo,
+            "PING": self.handle_ping,
+            "SET": self.handle_set,
+            "GET": self.handle_get,
+            "RPUSH": self.handle_rpush,
+        }
+    
+    
+    def execute(self, cmd, *args):
+        if cmd in self.commands:
+            return self.commands[cmd](*args)
+        else:
+            return f"-ERR unknown command '{cmd}'"
+    
+    
+    def handle_echo(self, message):
+        return message
+    
+    
+    def handle_ping(self, *args):
+        return "PONG"
+    
+    
+    def handle_set(self, *args):
+        key = args[0]
+        value = args[1]
+        options = args[2:] if len(args) > 2 else ()
+        return self.datastore.set(key, value, *options)
+    
+    
+    def handle_get(self, *args):
+        key = args[0]
+        return self.datastore.get(key)
+    
+    
+    def handle_rpush(self, *args):
+        key, *values = args
+        return self.datastore.rpush(key, *values)
+        
+
+class DataStore:
+    def __init__(self):
+        self.DATA = {}
+        self.EXPIRY = {}
+        self.LIST = {}
+    
+    
+    def set(self, key, value, *options):
+        try:
+            self.DATA[key] = value
+            print("Options:", options)
+            if options:
+                if options[0].upper() == "PX":
+                    self.EXPIRY[key] = (
+                        time.time() * 1000) + int(options[1]
+                    )
+            return "OK"
+        except Exception as e:
+            raise(e)
+    
+    
+    def get(self, key):
+        try:
+            value = self.DATA[key]
+            if key in self.EXPIRY and (time.time()*1000) > self.EXPIRY[key]:
+                del self.DATA[key]
+                del self.EXPIRY[key]
+                return None
+            else:
+                return value
+        except Exception as e:
+            raise(e)
+    
+    
+    def rpush(self, key, *values):
+        try:
+            if key in self.LIST:
+                self.LIST[key].extend(values)
+            else:
+                self.LIST[key] = list(values)
+            return len(self.LIST[key])
+        except Exception as e:
+            raise(e)
+
+
+class Server:
+    def __init__(self):
+        self.server_socket: socket = socket.create_server(("localhost", 6379), reuse_port=True)
+        self.datastore = DataStore()
+        self.cmd_handler = CommandHandler(self.datastore)
+    
+    def start(self):
+        while True:
+            client_socket, addr = self.server_socket.accept()
+            print(f"New connection from: {addr}")
+            thread = threading.Thread(target=self.handle_client, args=(client_socket,))
+            thread.start()
+    
+    def handle_client(self, client_socket: socket):
+        with client_socket:
+            while True:
+                raw = client_socket.recv(1024)
+                cmd, args = Parser.parse_request(request=raw)
+                response = self.cmd_handler.execute(cmd, *args)
+                print(response)
+                client_socket.sendall(Parser.format_response(response).encode())
+                
 
 def main():
     print("Logs from your program will appear here!")
     
-    # server
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    
-    # accepting connections: new thread for connection
-    while True:
-        conn, _ = server_socket.accept()
-        thread = threading.Thread(target=handle_client, args=(conn,))
-        thread.start()
+    redis_server = Server()
+    redis_server.start()
     
 
 if __name__ == "__main__":
